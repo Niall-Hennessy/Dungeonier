ac88f1c1b5b53d14523710568fe6f2bbf861b780
Xiqido <xiqido@gmail.com>
2022-02-19 18:22:48 +0000

    imagws

---
+++ TileMaps/!chests.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="!chests" tilewidth="16" tileheight="16" spacing="2" tilecount="168" columns="12">
+ <image source="../gfx/Package_Water_Dungeon/sprites/!chests.png" width="216" height="256"/>
+</tileset>
